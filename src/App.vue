1f1215dba0af5f82e7c2ca425f04ca12148101d2
jinterceptor <jensen.cocker@gmail.com>
2024-08-12 15:27:13 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -113,7 +113,7 @@ export default {
           "alias": "Caelan Meler",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
           "corpro": "GMS",
-          "frame": "Everest",
+          "frame": "Sagarmatha",
           "mech": "Act of Mercy"
         },
         {
