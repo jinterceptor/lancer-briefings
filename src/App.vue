4a11dbfe26e648b3850d6d1b4a28989e8faae8b5
jinterceptor <jensen.cocker@gmail.com>
2024-08-13 20:24:10 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -133,8 +133,8 @@ export default {
           "mech": "Basilisk"
         },
         {
-          "callsign": "Baron",
-          "alias": "Uknown",
+          "callsign": "Charon",
+          "alias": "Vladimir Prendes VI",
           "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
           "corpro": "GMS",
           "frame": "Everest",
