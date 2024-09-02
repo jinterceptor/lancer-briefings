fcd285a42d8f12c7aa13bfb2df06abcb2f5ddbee
jinterceptor <jensen.cocker@gmail.com>
2024-09-02 13:00:01 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -90,7 +90,7 @@ export default {
   data() {
     return {
       "mission_slug": "002",
-      "current_md": "start",
+      "current_md": "success",
       "events": "",
       "missions": [
         {
@@ -106,7 +106,7 @@ export default {
         {
           "slug": "002",
           "name": "Daybreak",
-          "status": "Start"
+          "status": "success"
         },
       ],
       "pilots": [
